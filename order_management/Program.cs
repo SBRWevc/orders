294815d07fcef7d58476dbbaa704a4d049edd9f7
Dmitriy <evc@sbrw.ru>
2024-01-23 16:46:08 +0300

    update data

--- order_management/Program.cs
+++ order_management/Program.cs
@@ -2,14 +2,12 @@ using test_task.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<OrderContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
